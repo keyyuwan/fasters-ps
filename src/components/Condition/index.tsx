effaa23c7a1019d3d60310e7d8bb9e82cb627a48
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-02-10 12:21:03 -0300

    Condition Desktop

--- src/components/Condition/index.tsx
+++ src/components/Condition/index.tsx
@@ -1,34 +1,39 @@
+import blackSuitImg from "../../assets/blackSuit.png";
 import styles from "./styles.module.scss";
 
 export function Condition() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>Condição imperdível</h1>
+        <div className={styles.conditions}>
+          <h1>Condição imperdível</h1>
 
-        <span>Compre hoje e garanta:</span>
+          <span>Compre hoje e garanta:</span>
 
-        <ul className={styles.guaranteesList}>
-          <li>
-            12% de desconto e pagamento em até <span>3x sem juros</span>;
-          </li>
-          <li>
-            Até <span>10 meses</span> para confeccionar sua roupa. Não se
-            preocupe se suas medidas aumentaram ou diminuíram, estará sempre
-            elegante;
-          </li>
-          <li>
-            Condição muito especial por <span>tempo limitado</span>;
-          </li>
-          <li>
-            Poder presentear alguém com uma roupa de alta costura,{" "}
-            <span>exclusiva</span>.
-          </li>
-        </ul>
+          <ul className={styles.guaranteesList}>
+            <li>
+              12% de desconto e pagamento em até <span>3x sem juros</span>;
+            </li>
+            <li>
+              Até <span>10 meses</span> para confeccionar sua roupa. Não se
+              preocupe se suas medidas aumentaram ou diminuíram, estará sempre
+              elegante;
+            </li>
+            <li>
+              Condição muito especial por <span>tempo limitado</span>;
+            </li>
+            <li>
+              Poder presentear alguém com uma roupa de alta costura,{" "}
+              <span>exclusiva</span>.
+            </li>
+          </ul>
 
-        <button className={styles.button}>
-          Garanta essa condição especial!
-        </button>
+          <button className={styles.button}>
+            Garanta essa condição especial!
+          </button>
+        </div>
+
+        <img src={blackSuitImg} alt="Terno preto" />
       </div>
     </div>
   );
