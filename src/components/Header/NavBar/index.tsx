74ccfd94cd0dba49dbcd5894286e71028978a7e6
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-02-10 21:05:03 -0300

    added href="#" to anchor tags

--- src/components/Header/NavBar/index.tsx
+++ src/components/Header/NavBar/index.tsx
@@ -3,11 +3,11 @@ import styles from "./styles.module.scss";
 export function NavBar() {
   return (
     <nav className={styles.navBar}>
-      <a>Empresa</a>
-      <a>Camisaria</a>
-      <a>Alfaiataria</a>
-      <a>Depoimentos</a>
-      <a>Contato</a>
+      <a href="#">Empresa</a>
+      <a href="#">Camisaria</a>
+      <a href="#">Alfaiataria</a>
+      <a href="#">Depoimentos</a>
+      <a href="#">Contato</a>
     </nav>
   );
 }
