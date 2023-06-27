247099e373672d770667d4f8e8ada6b3d2e58e04
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-06-27 10:31:07 -0300

    Update index.tsx

--- src/components/WhatsAppButton/index.tsx
+++ src/components/WhatsAppButton/index.tsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 export function WhatsAppButton() {
   return (
     <div className={styles.container}>
-      <a href="https://api.whatsapp.com/send?phone=554199946727" style={{ textDecoration: 'none' }}>
+      <a href="https://api.whatsapp.com/send?phone=554199946727" style={{ textDecoration: 'none' }} target="_blank">
       
       <img src={whatsAppImg} alt="WhatsApp logo" />
       </a>
