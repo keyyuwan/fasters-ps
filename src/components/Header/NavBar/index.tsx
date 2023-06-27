9fa31d325230935c29b896c13a6eb873d09d2029
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-06-27 10:02:19 -0300

    Update index.tsx

--- src/components/Header/NavBar/index.tsx
+++ src/components/Header/NavBar/index.tsx
@@ -3,11 +3,11 @@ import styles from "./styles.module.scss";
 export function NavBar() {
   return (
     <nav className={styles.navBar}>
-      <a href="/#">Empresa</a>
-      <a href="/#">Camisaria</a>
-      <a href="/#">Alfaiataria</a>
-      <a href="/#">Depoimentos</a>
-      <a href="/#">Contato</a>
+      <a href="#empresa">Empresa</a>
+      <a href="#camisaria">Camisaria</a>
+      <a href="#alfaiataria">Alfaiataria</a>
+      <a href="#depoimentos">Depoimentos</a>
+      <a href="#contato">Contato</a>
     </nav>
   );
 }
